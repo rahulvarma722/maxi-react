773a07163e597752ad0e7cd4f4e1d119f64e8526
rahulvarma722 <rahulvarma722@gmail.com>
2022-06-19 16:37:59 +0530

    25 binaries uploaded

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 // import App from './App';
+// Add style componet
 import App from './styleComponent/src/App';
+
+// Add styled by css module system
+import App from './styleComponentCss/src/App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
