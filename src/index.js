9f847e9b15dafae5239b847c2b5d0f680ba12255
rahulvarma722 <rahulvarma722@gmail.com>
2022-06-19 15:10:23 +0530

    Complete styled component and start Css module addin css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+// import App from './App';
+import App from './styleComponent/src/App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
